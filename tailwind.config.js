0b257264508bd3aae0a837f3140c4bc2a8b3c2db
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-07-10 16:22:11 +0100

    added custom keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 };

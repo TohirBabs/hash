d29c78d0aee66246600820c3e2da3e711f9dd4e0
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-07-10 16:22:53 +0100

    Updated profile picure component

--- src/components/pfp/Pfp.tsx
+++ src/components/pfp/Pfp.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
 import React from "react";
+import { StatusProgress } from "./StatusProgress";
 
 export const Pfp = () => {
-  return <div>Pfp</div>;
+  return (
+    <div className="relative flex items-center justify-center overflow-hidden">
+      <div className="w-12 h-12 absolute bg-black rounded-full">
+        <Image
+          src="/assets/images/selfie.jpg"
+          alt="profile picture"
+          fill
+          objectFit="cover"
+        />
+      </div>
+
+      <StatusProgress />
+    </div>
+  );
 };

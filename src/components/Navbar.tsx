fe1554ae0dfe6537802b48f10c6250b5663aec18
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-07-20 01:14:39 +0100

    fix: remove console.log

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,6 @@ import UserAccountNav from "./UserAccountNav";
 
 export const Navbar = async () => {
   const session = await getAuthSession();
-  console.log(session);
 
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">

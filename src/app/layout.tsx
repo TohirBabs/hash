3f71f07c908621e2d419fdbd5a0a4a6a3d514ab1
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-07-27 10:45:07 +0100

    wrap app in provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,31 +4,37 @@ import type { Metadata } from "next";
 import { Comfortaa } from "next/font/google";
 import { Navbar } from "../components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hash",
-  description: "Social media",
+  title: "hash",
+  description: "An online forum for the nigerian community",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: // authModal,
+{
   children: React.ReactNode;
+  // authModal: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
       className={cn("bg-white text-slate-900", comfortaa.className)}
     >
-      <body className="min-h-screen pt-12 bg-slate-50">
-        <Navbar />
-        <main className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </main>
-        <Toaster />
-      </body>
+      <Providers>
+        <body className="min-h-screen pt-12 bg-slate-50">
+          <Navbar />
+          {/* {authModal} */}
+          <main className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </main>
+          <Toaster />
+        </body>
+      </Providers>
     </html>
   );
 }
